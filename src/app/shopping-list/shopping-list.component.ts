614081c3d5b250bfbab65edd2741dd9b92e19dba
Lawrence Liu <Lawrencehudson23@gmail.com>
2020-05-12 14:37:57 -0700

    finish

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,7 +1,12 @@
+import { Ingredient } from '../shared/ingredient.model';
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
 })
-export class ShoppingListComponent {}
+export class ShoppingListComponent {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apple', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
+}
